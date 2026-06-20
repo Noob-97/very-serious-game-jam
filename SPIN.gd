819379b41f994fb349d782bae9f944bc9a2b86ee
SammyBoi <leongjiahao10@gmail.com>
2026-06-20 15:52:00 +0800

    Better prototype in Test 3.

--- SPIN.gd
+++ SPIN.gd
@@ -1,11 +1,8 @@
 extends Node2D
-var deg = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var rotation_speed: float = 60.0
 
+@export_enum("Clockwise:1", "Counter-Clockwise:-1") var spin_direction: int = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	rotate(deg_to_rad(deg))
+func _physics_process(delta: float) -> void:
+	rotate(deg_to_rad(rotation_speed) * delta)
--- player.gd
+++ player.gd
@@ -1,11 +1,36 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -500.0
 @export var text: Label
-var life = 100
+var life = 5
+
+var invincible_timer: float = 0.0
+
+func _ready() -> void:
+	floor_snap_length = 5.0
+	floor_max_angle = deg_to_rad(60.0)
+	if text:
+		text.text = "LIFE: " + str(life)
 
 func _physics_process(delta: float) -> void:
+	if invincible_timer > 0:
+		invincible_timer -= delta
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("JumpKey") and (is_on_floor() or is_on_wall()):
+		velocity.y = JUMP_VELOCITY
+	
+	var direction := Input.get_axis("LeftKey", "RightKey")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
+	
 	# KILL
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
@@ -13,26 +38,13 @@ func _physics_process(delta: float) -> void:
 		
 		# Check if we hit the static body
 		if collider is StaticBody2D:
-			if collider.name.contains("kill"):
-				life -= 1
-				text.text = "LIFE: " + str(life)
+			if collider.is_in_group("kill") and invincible_timer <= 0:
+				life -= 5
+				invincible_timer = 1.0
+				
+				if text:
+					text.text = "LIFE: " + str(life)
+					
 				if life <= 0:
 					get_tree().reload_current_scene()
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
